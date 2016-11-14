4b6389c517395df49098d60dbebde919b46ee03f
Scott Warner <Tortel1210@gmail.com>
2016-11-14 06:57:20 -0500

    x2: Include stlport
    It looks like some camera libs may need it

--- device.mk
+++ device.mk
@@ -190,14 +190,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PACKAGES += \
     Snap \
-    libcamera_parameters_shim \
     libcamera_shim \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service.oneplus3 \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    libstlport
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
