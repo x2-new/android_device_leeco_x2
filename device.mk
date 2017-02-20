5e9eb5f5c244aa30b59176c5c0201c800b713442
Scott Warner <Tortel1210@gmail.com>
2017-02-19 19:47:45 -0500

    x2: Include sensor conf
    Change-Id: I24467048893c150445b176fc75a52556e1870d10

--- device.mk
+++ device.mk
@@ -435,6 +435,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# Dalvik and hwui configs
-$(call inherit-product, frameworks/native/build/phone-xxxhdpi-4096-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxxhdpi-4096-hwui-memory.mk)
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:system/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
+
+# msm8996-common
+$(call inherit-product, device/leeco/msm8996-common/msm8996.mk)
