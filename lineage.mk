31576ce7baa4aa37db9d780589d8f307d444b3e3
andr68rus <ashtmb@gmail.com>
2017-11-12 12:34:49 +0300

    x2: change vendor/cm to vendor/lineage
    Change-Id: Ib62f81930092d04d77df5c541e4b7830e4476f1f

--- lineage.mk
+++ lineage.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/leeco/x2/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := cm_x2
