79506d4d54342cec00d2ca1ced05e56ccd4b2536
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-11-12 12:52:53 +0300

    x2: lineagehw: rebrand
    Change-Id: Id2fd465d3291347009565105b97189ae0fc479c5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -212,8 +212,8 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_op3
 TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 
-# CM Hardware
-BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/cmhw
+# Lineage Hardware
+BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/lineagehw
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
